5a3d8327eaeaec58a4a6d6cd7dbf67d89fa24c0e
PraveenPriyesh <129766701+PraveenPriyesh@users.noreply.github.com>
2026-02-22 01:42:46 +0530

    updated

--- SampleApi.js
+++ SampleApi.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const fs = require('fs');
 
 const app = express();
 
@@ -9,6 +10,20 @@ app.use(cors());
 const data = "Sutha Mudittu Po da Thayoli !!!    - Praveen Narayanan";
 
 app.get('/app/data', (req, res) => {
+
+    // IP address capture
+    const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
+    const userAgent = req.headers['user-agent'];
+
+    console.log("IP:", ip);
+    console.log("Device:", userAgent);
+    console.log("Time:", new Date().toISOString());
+    console.log("--------------------------------");
+
+    // Optional: save into file
+    const log = `IP: ${ip} | Time: ${new Date().toISOString()} | Device: ${userAgent}\n`;
+    fs.appendFileSync('logs.txt', log);
+
     res.json(data);
 });
 
