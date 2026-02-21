6c32e454c927015cb250116babe6b0b744bb8fdd
PraveenPriyesh <129766701+PraveenPriyesh@users.noreply.github.com>
2026-02-22 00:17:27 +0530

    Final

---
+++ SampleApi.js
@@ -0,0 +1,16 @@
+const express=require('express');
+const cops = require('cors');
+const PORT = 7001;
+const app = express();
+app.use(express.json());
+app.use(cops());
+const data = [{
+    Name : 'Praveen',
+    Age : '25'
+}]
+app.get('/app/data' , (req , res)=>{
+    res.json(data);
+})
+app.listen(PORT,()=>{
+    console.log('Successfully running on the port');
+})
