a7bdc8b3e04435e82740a898a53e9de5525aa5f1
PraveenPriyesh <129766701+PraveenPriyesh@users.noreply.github.com>
2026-02-22 00:31:39 +0530

    Updated

--- SampleApi.js
+++ SampleApi.js
@@ -1,17 +1,22 @@
-const express=require('express');
-const cops = require('cors');
-const PORT = 7001;
+const express = require('express');
+const cors = require('cors');
+
 const app = express();
+
 app.use(express.json());
-app.use(cops());
+app.use(cors());
+
 const data = [{
-    Name : 'Praveen',
-    Age : '25'
-}]
-app.get('/app/data' , (req , res)=>{
+    Name: 'Praveen',
+    Age: '25'
+}];
+
+app.get('/app/data', (req, res) => {
     res.json(data);
-})
+});
+
 const PORT = process.env.PORT || 7001;
-app.listen(PORT,()=>{
-    console.log('Successfully running on the port');
-})
+
+app.listen(PORT, () => {
+    console.log(`Successfully running on port ${PORT}`);
+});
